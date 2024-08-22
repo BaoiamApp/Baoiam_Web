b771190aa1376b30966fecfc2566d76b55b86919
Aayush-1205 <shindeaayush250@gmail.com>
2024-08-22 12:45:54 +0530

    Updated PAP page

--- static/PAP/js/script.js
+++ static/PAP/js/script.js
@@ -5,6 +5,16 @@ var swiper = new Swiper('.blog-slider', {
   mousewheel: {
     invert: false,
   },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
   // autoHeight: true,
   pagination: {
     el: '.blog-slider__pagination',
